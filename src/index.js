2a2b7e767aed3f42e852aa6a030cc689a8820d96
Brendan Rielly <brielly@backcountry.com>
2017-08-14 15:50:59 -0600

    Linting - Module 9

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import '../index.css';
-import numeral from 'numeral';
+// import '../index.css';
+// import numeral from 'numeral';
 
 
-const courseValue = numeral(1000).format('$0,00.00');
-debugger;
-console.log(`I would pay ${courseValue} for this awesome course!`);
+// const courseValue = numeral(1000).format('$0,00.00');
+// console.log(`I would pay ${courseValue} for this awesome course!`);
